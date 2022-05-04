7d776dac2c7ae48d584f9eb241df434b3df9b2b9
Muhammad Ahmad <ahmadbutt4260@gmail.com>
2022-05-04 13:53:37 +0500

    cube

--- chap2/src/World/components/cube.js
+++ chap2/src/World/components/cube.js
@@ -1,8 +1,8 @@
-import { BoxBufferGeometry, Mesh, MeshBasicMaterial } from "three";
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial, Color } from "three";
 
 function createCube() {
   const geometry = new BoxBufferGeometry(2, 2, 2);
-  const material = new MeshBasicMaterial();
+  const material = new MeshStandardMaterial();
 
   const cube = new Mesh(geometry, material);
 
